c0dbb32a8ba27ab1161bb924b2097dad6d04b32b
ktkx <36291027+ktkx@users.noreply.github.com>
2018-02-12 14:18:47 +0800

    Add files via upload

---
+++ System.cpp
@@ -0,0 +1,29 @@
+// System.cpp
+
+#include "System.h"
+using namespace std;
+
+System::System() {
+	// read in file
+	// store in vectors?
+}
+
+void System::encryptData() {
+	
+}
+void System::decryptData() {
+	
+}
+
+void System::displayLogin() {
+	
+}
+void System::displayFail(){cout<<"Invalid Username or Password!" << endl;};
+
+int System::displayMainMenu() {
+	
+}
+
+void System::displayEditDetails() {
+	
+}
