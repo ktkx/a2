7b0abd38ea819700bde33d823b8852e270759df9
badain1242 <36290996+badain1242@users.noreply.github.com>
2018-02-12 15:03:18 +0800

    Add files via upload

--- StockList.h
+++ StockList.h
@@ -8,21 +8,21 @@ class StockList {
 	public:
 		StockList();
 		void createNewRecord(Stock);
-		void updateStock();
 		void displayStock();
-		void provideStockAlerts();
-		void searchStock();
+		void provideStockAlerts(int);
+		void searchStockByCategory(string);
+		void searchStockByPrice(int);
+		void searchStockByQuantity(int);
+		void updateStock(Stock, int);
 		void removeStock(string);
 		
+		//Sort ascending
 		void sortPriceAscending();
-
 		void sortQuantityAscending();
-
 		void sortCategoryAscending();
-
 		void sortSubCategoryAscending();
 
-
+		//Sort descending
 		void sortPriceDescending();
 		void sortQuantityDescending();
 		void sortCategoryDescending();
